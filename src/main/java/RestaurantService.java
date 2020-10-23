a13160fcaab889c6a6b38e24751022606500d763
Utkarsh Singh <utkarsh.singh@upgrad.com>
2020-10-23 14:16:52 +0530

    Added comments

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -1,7 +1,5 @@
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 public class RestaurantService {
--- src/test/java/RestaurantServiceTest.java
+++ src/test/java/RestaurantServiceTest.java
@@ -1,9 +1,7 @@
 import org.junit.jupiter.api.*;
-import org.mockito.Mockito;
 import java.time.LocalTime;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doReturn;
 
 
 class RestaurantServiceTest {
@@ -20,6 +18,7 @@ class RestaurantServiceTest {
         restaurant.addToMenu("Vegetable lasagne", 269);
     }
 
+    //>>>>>>>>>>>>>>>>>>>>>>SEARCHING<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @Test
     public void searching_for_existing_restaurant_should_not_return_null() {
         Restaurant searchedRestaurant = service.findRestaurantByName("Amelie's cafe");
@@ -30,7 +29,11 @@ class RestaurantServiceTest {
         Restaurant searchedRestaurant = service.findRestaurantByName("Pantry d'or");
         assertNull(searchedRestaurant);
     }
+    //<<<<<<<<<<<<<<<<<<<<SEARCHING>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>ORDER<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @Test
     public void calculateOrderTotal_should_throw_exception_if_item_not_found(){
         assertThrows(itemNotFoundException.class,
@@ -41,16 +44,17 @@ class RestaurantServiceTest {
     assertEquals(388,restaurant.calculateOrderTotal("Sweet corn soup","Vegetable lasagne"));
     }
     //------------------------------------------------------------------------------
+    //<<<<<<<<<<<<<<<<<<<<ORDER>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
-    //ADMIN ROLE
+    //>>>>>>>>>>>>>>>>>>>>>>ADMIN: ADDING & REMOVING RESTAURANTS<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     @Test
     public void remove_restaurant_should_reduce_list_of_restaurants_size_by_1() throws restaurantNotFoundException {
         int initialNumberOfRestaurants = service.getRestaurants().size();
         service.removeRestaurant("Amelie's cafe");
         assertEquals(initialNumberOfRestaurants-1, service.getRestaurants().size());
     }
-    
+
     @Test
     public void removing_restaurant_that_does_not_exist_should_throw_exception() throws restaurantNotFoundException {
         assertThrows(restaurantNotFoundException.class,()->service.removeRestaurant("Pantry d'or"));
@@ -62,5 +66,5 @@ class RestaurantServiceTest {
         service.addRestaurant("Pumpkin Tales","Chennai",LocalTime.parse("12:00:00"),LocalTime.parse("23:00:00"));
         assertEquals(initialNumberOfRestaurants + 1,service.getRestaurants().size());
     }
-    //-------------------------------------------------------
+    //<<<<<<<<<<<<<<<<<<<<ADMIN: ADDING & REMOVING RESTAURANTS>>>>>>>>>>>>>>>>>>>>>>>>>>
 }
